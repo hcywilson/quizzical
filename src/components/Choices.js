7a6298f8021579fec74fd254f9ff4d902a2f7045
hcywilson <hcywilson0920@gmail.com>
2022-03-28 15:50:31 -0400

    add deployment

---
+++ src/components/Choices.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Choices() {
+    const a = {
+        correct_answer: "Slam dunk, nothing but net!",
+        incorrect_answers: [
+            "Hit the sack, Jack!",
+            "Rikki-Tikki-Tavi, biatch!",
+            "Wubba-lubba-dub-dub!"
+        ]
+    }
+
+    
+}
