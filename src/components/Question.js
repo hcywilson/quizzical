63cd26344387e5f09e9122a78e6de0b8696e5c2f
hcywilson <hcywilson0920@gmail.com>
2022-03-29 17:23:14 -0400

    clean up css

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,55 +1,52 @@
 import React from "react";
 
 export default function Question(props) {
-
-
-
-    const choiceElements = props.choices.map(choice => {
-        let style
-        if (props.gameStage === "play") {
-            style = {
-                cursor: "pointer"
-            }
-            if (choice === props.chosen) {
-                style.backgroundColor = "#D6DBF5"
-                style.fontWeight = '700'
-
-            }
-            else {
-                style.backgroundColor = "#ffffff"
-                style.border = "0.794239px solid #4D5B9E"
-            }
-        } else {
-            if (choice === props.correctAnswer) {
-                style = {
-                    backgroundColor: "#94D7A2",
-                    fontWeight: 700
-                }
-            } else if (choice === props.chosen) {
-                style = {
-                    backgroundColor: "#F8BCBC",
-                    opacity: 0.6
-                }
-            } else style = {
-                backgroundColor: "#ffffff",
-                border: "0.794239px solid #4D5B9E",
-                opacity: 0.6
-            }
-        }
-        return <button
-            className="question--choice"
-            style={style}
-            key={choice}
-            onClick={() => props.gameStage === "play" && props.chooseAnswer(choice)}
-            dangerouslySetInnerHTML={{ __html: choice }}>
-        </button>
-    })
+  const choiceElements = props.choices.map((choice) => {
+    let style;
+    if (props.gameStage === "play") {
+      style = {
+        cursor: "pointer",
+      };
+      if (choice === props.chosen) {
+        style.backgroundColor = "#D6DBF5";
+      } else {
+        style.backgroundColor = "#ffffff";
+        style.border = "0.794239px solid #4D5B9E";
+      }
+    } else {
+      if (choice === props.correctAnswer) {
+        style = {
+          backgroundColor: "#94D7A2",
+        };
+      } else if (choice === props.chosen) {
+        style = {
+          backgroundColor: "#F8BCBC",
+          opacity: 0.6,
+        };
+      } else
+        style = {
+          backgroundColor: "#ffffff",
+          border: "0.794239px solid #4D5B9E",
+          opacity: 0.6,
+        };
+    }
     return (
-        <div className="question">
-            <p className="question--question" dangerouslySetInnerHTML={{ __html: props.question }} />
-            <div className="question--choices">
-                {choiceElements}
-            </div>
-        </div>
-    )
+      <button
+        className="question--choice"
+        style={style}
+        key={choice}
+        onClick={() => props.gameStage === "play" && props.chooseAnswer(choice)}
+        dangerouslySetInnerHTML={{ __html: choice }}
+      ></button>
+    );
+  });
+  return (
+    <div className="question">
+      <p
+        className="question--question"
+        dangerouslySetInnerHTML={{ __html: props.question }}
+      />
+      <div className="question--choices">{choiceElements}</div>
+    </div>
+  );
 }
